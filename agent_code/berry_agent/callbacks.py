dbd85ad5b462beae8e3a1259b9e020f97c012f57
maiqizhou <zhoumaiqi@yeah.net>
2023-09-11 19:32:49 +0200

    first agent
    work done by berry

---
+++ agent_code/berry_agent/callbacks.py
@@ -0,0 +1,68 @@
+import numpy as np
+import os, pickle
+import torch
+from agent_code.berry_agent.lstm import LSTMModel
+learning_rate = 0.001
+
+def setup(self):
+    # print(self)
+    # if self.train or not os.path.isfile("my-saved-model.pt"):
+    #     self.logger.info("Setting up model from scratch.")
+    #     self.model = LSTMModel(17*17, 32, 1, 5)
+    # else:
+    #     self.logger.info("Loading model from saved state.")
+    #     with open("my-saved-model.pt", "rb") as file:
+    #         self.model = pickle.load(file)
+    self.logger.info("Setting up model from scratch.")
+    self.model = LSTMModel(17*17, 32, 1, 5)
+    self.optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
+    self.model.train()
+
+def act(agent, game_state: dict):
+    # print(agent)
+    # raise "123"
+    agent.logger.info('Pick action at random, but no bombs.')
+    actions = ['RIGHT', 'LEFT', 'UP', 'DOWN', "WAIT"]
+    map = state_to_features(game_state).reshape(1, 1, -1)
+    map = torch.tensor(map)
+    probability = agent.model(map)
+    action = actions[torch.argmax(probability)]
+    return action
+
+
+
+
+def state_to_features(game_state: dict) -> np.array:
+    """
+    *This is not a required function, but an idea to structure your code.*
+
+    Converts the game state to the input of your model, i.e.
+    a feature vector.
+
+    You can find out about the state of the game environment via game_state,
+    which is a dictionary. Consult 'get_state_for_agent' in environment.py to see
+    what it contains.
+
+    :param game_state:  A dictionary describing the current game board.
+    :return: np.array
+    """
+    # This is the dict before the game begins and after it ends
+    if game_state is None:
+        return None
+
+    # For example, you could construct several channels of equal shape, ...
+    channels = game_state["field"]
+    for x, y in game_state["coins"]:
+        channels[x][y] = 3
+    for other in game_state["others"]:
+        if other[2]:
+            x, y = other[3]
+            channels[x][y] = 2
+    x, y = game_state["self"][-1]
+    channels[x][y] = 1
+
+    # channels.append(...)
+    # concatenate them as a feature tensor (they must have the same shape), ...
+    stacked_channels = np.stack(channels)
+    # and return them as a vector
+    return stacked_channels.reshape(-1)
---
+++ agent_code/berry_agent/lstm.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+
+
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(LSTMModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+        self.fl = nn.Linear(output_size, output_size)
+
+    def forward(self, x):
+        x = x.to(torch.float)
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        out = self.fl(out)
+        return out
